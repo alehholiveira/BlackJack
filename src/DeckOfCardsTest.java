0310c91be64bb6e0b1dc0dbfbf69dfdd30522d84
Alexandre <aletoscaro@gmail.com>
2023-09-24 17:51:45 -0300

    criacao funcao para jogadores

--- src/DeckOfCardsTest.java
+++ src/DeckOfCardsTest.java
@@ -1,84 +1,45 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DeckOfCardsTest {
    // execute application
    public static void main(String[] args) {
-
+      clearConsole();
       Scanner input = new Scanner(System.in);
-
-      int verificadorsim;
       int count = 2;
 
+      System.out.println("Seja bem vindo ao nosso BLACKJACK!!");
+      System.out.print("Quantas pessoas irão jogar? ");
+      int numPlayers = input.nextInt();
+      Card[][] playerHands = new Card[numPlayers][];
+
       DeckOfCards baralho = new DeckOfCards();
-      baralho.shuffle(); // place Cards in random order
-      // print all 52 Cards in the order in which they are dealt
-      clearConsole();
-      System.out.printf("Carta inicialmente distribuidas -> ");
-      for (int i = 1; i <= 2; i++) {
-         System.out.printf(" | %s |", baralho.dealCard());
+      baralho.shuffle();
+      for (int jogador = 0; jogador < numPlayers; jogador++) {
+
+          System.out.printf("Jogador %d: ", jogador + 1);
+          playerHands[jogador] = jogo(count, baralho, jogador);
 
       }
-      do {
-         verificadorsim = 0;
-         System.out.printf("%nDeseja receber mais uma carta? ");
-         String option = input.nextLine();
-         String optionUP = option.toLowerCase();
-         System.out.println();
-         if (optionUP.equals("sim")) {
-            System.out.printf("NOVA CARTA DISTRIBUIDA -> | %s |", baralho.dealCard());
-            verificadorsim++;
-            count++;
-         }
-      } while (verificadorsim != 0);
+        // Determinar o jogador vencedor
+        int maiorPontuacao = -1; // Inicialize com um valor negativo
+        int jogadorVencedor = -1;
 
-      Card[] playerCards = baralho.getCurrentCard(); // Obtém as cartas do jogador
+        for (int jogador = 0; jogador < numPlayers; jogador++) {
+            int soma = baralho.ValueCard(Arrays.copyOf(playerHands[jogador], count), count);
+            if (soma <= 21 && soma > maiorPontuacao) {
+                maiorPontuacao = soma;
+                jogadorVencedor = jogador;
+            }
+        }
 
-      System.out.print("TODAS AS CARTAS -> | ");
-      for (int i = 0; i < count; i++) {
-         System.out.printf("%s | ", playerCards[i]);
-      }
-      int soma = baralho.ValueCard(playerCards, count, 0); // Corrige a chamada do método
-      System.out.printf("%nSoma das cartas: %d%n", soma); // Corrige a formatação da saída
-      System.out.println(); // Para criar uma nova linha após a impressão das cartas
-
-      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      String option = input.nextLine();
-      int count2 = count;
-
-
-      // print all 52 Cards in the order in which they are dealt
-      clearConsole();
-      System.out.printf("Carta inicialmente distribuidas -> ");
-      for (int i = 1; i <= 2; i++) {
-         System.out.printf(" | %s |", baralho.dealCard());
-
-      }
-      do {
-         verificadorsim = 0;
-         System.out.printf("%nDeseja receber mais uma carta? ");
-         option = input.nextLine();
-         String optionUP = option.toLowerCase();
-         System.out.println();
-         if (optionUP.equals("sim")) {
-            System.out.printf("NOVA CARTA DISTRIBUIDA -> | %s |", baralho.dealCard());
-            verificadorsim++;
-            count2++;
-         }
-      } while (verificadorsim != 0);
-
-      Card[] playerCards2 = baralho.getCurrentCard(); // Obtém as cartas do jogador
-
-      System.out.print("TODAS AS CARTAS -> | ");
-      for (int i = count; i < count2 + 2; i++) {
-         System.out.printf("%s | ", playerCards2[i]);
-      }
-      int soma2 = baralho.ValueCard(playerCards2, count, count2); // Corrige a chamada do método
-      System.out.printf("%nSoma das cartas: %d%n", soma2); // Corrige a formatação da saída
-      System.out.println(); // Para criar uma nova linha após a impressão das cartas
-
-      input.close();
+        if (jogadorVencedor != -1) {
+            System.out.printf("Jogador %d venceu com %d pontos!%n", jogadorVencedor + 1, maiorPontuacao);
+        } else {
+            System.out.println("Nenhum jogador venceu. Todos estouraram o limite.");
+        }
    }
-
+      
    public static void clearConsole() { // FUNCAO PARA LIMPAR O CONSOLE DO USUARIO
       try {
           final String os = System.getProperty("os.name");
@@ -94,4 +55,52 @@ public class DeckOfCardsTest {
       } catch (Exception e) {
       }
   }
-} // end class DeckOfCardsTest
+
+   public static Card[] jogo(int count, DeckOfCards baralho, int jogador) {
+      Scanner input = new Scanner(System.in);
+      int verificadorsim;
+      int novacarta = 0;
+      int soma = 0;
+
+      // Crie um array de cartas para representar a mão do jogador
+      Card[] playerHand = new Card[51];
+
+      System.out.printf("Carta inicialmente distribuídas -> ");
+      for (int i = 0; i < count; i++) {
+         playerHand[i] = baralho.dealCard(); // Distribua cartas para a mão do jogador
+         System.out.printf(" | %s |", playerHand[i]);
+      }
+      
+      do {
+         Card[] playerCards = Arrays.copyOf(playerHand, count); // Copie o array da mão do jogador
+         soma = baralho.ValueCard(playerCards, count); // Corrige a chamada do método
+         System.out.printf(" -  Pontuação: %d%n", soma); // Corrige a formatação da saída
+         if (soma > 21) {
+            System.out.printf("Você estourou o limite!!%n%n");
+            break; // Saia do loop se a soma for maior que 21
+         }
+         if (soma == 21) {
+            System.out.println("Você atingiu 21!!%n%n");
+            break; // Saia do loop se a soma for maior que 21
+         }
+         verificadorsim = 0;
+         System.out.printf("Jogador %d: Deseja receber mais uma carta? ", jogador + 1);
+         String option = input.nextLine();
+         String optionUP = option.toLowerCase();
+         System.out.println();
+         if (optionUP.equals("sim")) {
+            novacarta++;
+            playerHand[count] = baralho.dealCard(); // Distribua uma carta adicional
+            System.out.printf("Jogador %d: Nova carta distribuída -> | %s |", jogador + 1, playerHand[count]);
+            System.out.printf("%nJogador %d: Cartas atualmente distribuídas -> ", jogador + 1);
+            for (int i = 0; i < count + 1; i++) {
+               System.out.printf(" | %s |", playerHand[i]);
+            }
+            verificadorsim++;
+            count++;
+         }
+      } while (verificadorsim != 0);
+
+      return playerHand; // Retorne o array de cartas do jogador
+   }
+}
